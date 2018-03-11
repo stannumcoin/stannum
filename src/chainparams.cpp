7bdb0e40763429bc6c627f32ed3003372bc3f5ca
stannumcoin <37253888+stannumcoin@users.noreply.github.com>
2018-03-11 13:49:48 +0300

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -65,7 +65,7 @@ public:
     CMainParams() {
         strNetworkID = "main";
         consensus.nSubsidyHalvingInterval = 262800; // one year
-        consensus.nMasternodePaymentsStartBlock = 3000; // not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
+        consensus.nMasternodePaymentsStartBlock = 1500; // not true, but it's ok as long as it's less then nMasternodePaymentsIncreaseBlock
         consensus.nMasternodePaymentsIncreaseBlock = 158000; // not used
         consensus.nMasternodePaymentsIncreasePeriod = 576*30; // not used
         consensus.nInstantSendKeepLock = 24;
